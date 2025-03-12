17515e2275b8a15ea83f5ae9f705900bd0a0f580
mustafaxv7 <mustafanadour40@gmail.com>
2025-03-12 22:17:33 +0100

    Our project boilerplate

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+
+function authMiddleware(req,res,next){
+
+};
+
+
+export default authMiddleware;
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+
+router.post('/register',(req,res)=>{
+
+});
+
+router.post('/login', (req,res)=>{
+
+});
+
+export default router;
