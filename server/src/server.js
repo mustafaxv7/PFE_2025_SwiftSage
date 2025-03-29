5d926f0853cc04d6d207886fba5514666f147abf
mustafaxv7 <mustafanadour40@gmail.com>
2025-03-29 15:40:01 +0100

    push regular changes when we rebuild the project

--- server/src/server.js
+++ server/src/server.js
@@ -3,8 +3,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path , {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import path from 'path';
 import con from './config/db.js';
 import authRoutes from './routes/authRoutes.js';       
 
@@ -12,8 +11,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5030;
-
-// const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.resolve();
 
@@ -24,7 +21,7 @@ app.use(helmet({contentSecurityPolicy: false})); // helps secure the app by sett
 app.use(morgan("dev")); // log the requests
 app.use(cors()); // allows requests from diffrent domains
 app.use('/auth',authRoutes);
-console.log(__dirname);
+
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, 'client','dist','index.html'));
 });
