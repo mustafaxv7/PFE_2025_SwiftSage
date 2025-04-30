3c2a5f76f1afcf334e4f422513284cfae154948d
mustafaxv7 <mustafanadour40@gmail.com>
2025-04-30 17:03:05 +0200

    add the new routes get report details for admin and user

--- server/src/routes/reportRoutes.js
+++ server/src/routes/reportRoutes.js
@@ -5,14 +5,22 @@ import adminMiddleware from '../middleware/adminMiddleware.js';
 import { addReport } from '../controllers/addReport.js';
 import { switchStatus } from '../controllers/switchSatus.js';
 import { editReport } from '../controllers/editReport.js';
+import { getReportDetailsAdmin } from '../controllers/getReportDetailsAdmin.js';
+import { getReportDetailsUser } from '../controllers/getReportDetailsUser.js';
 import e from 'express';
 dotenv.config();
 
 const router = express.Router();
 
+//user routes
 router.post('/', addReport);
 
+router.get('/:id/user', authMiddleware, getReportDetailsUser); 
+
 router.patch('/:id/edit', authMiddleware,editReport);
+
+//admin routes
+router.get('/:id', authMiddleware, adminMiddleware, getReportDetailsAdmin);
 
 router.patch('/:id/status', authMiddleware,adminMiddleware,switchStatus);
 
