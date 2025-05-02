2ae47e930c42c074a2510295c436051f6eab15eb
mustafaxv7 <mustafanadour40@gmail.com>
2025-05-02 19:44:05 +0200

    host DB to Neon Database succesefully

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,11 +5,12 @@ const { Client } = pkg;
 dotenv.config();
 
 const con = new Client({
-    host: "localhost",
-    user: "postgres",
+    host: process.env.DB_CONNECTION_HOST, 
+    user: "swiftsage_owner",
     port: 5432,
     password: process.env.DB_CONNECTION_PASSWORD,
-    database: "swiftsage"
+    database: "swiftsage",
+    ssl: { rejectUnauthorized: false } // Neon requires SSL
 });
 
 export default con;
